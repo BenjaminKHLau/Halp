19a410c335800c23fe2b3c8de058d8fc9aa4c1ef
Benjamin Lau <benjaminkhlau@gmail.com>
2022-10-16 19:28:21 -0500

    trying to add forms

---
+++ app/forms/business_hours.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, DateField, TimeField
+from wtforms.validators import DataRequired
+
+class BusinessHoursForm(FlaskForm):
+    SundayOpen = TimeField("Sunday Open", validators=[DataRequired()])
+    SundayOpen = TimeField("Sunday Close", validators=[DataRequired()])
+    MondayOpen = TimeField("Monday Open", validators=[DataRequired()])
+    MondayOpen = TimeField("Monday Close", validators=[DataRequired()])
+    TuesdayOpen = TimeField("Tuesday Open", validators=[DataRequired()])
+    TuesdayOpen = TimeField("Tuesday Close", validators=[DataRequired()])
+    WednesdayOpen = TimeField("Wednesday Open", validators=[DataRequired()])
+    WednesdayOpen = TimeField("Wednesday Close", validators=[DataRequired()])
+    ThursdayOpen = TimeField("Thursday Open", validators=[DataRequired()])
+    ThursdayOpen = TimeField("Thursday Close", validators=[DataRequired()])
+    FridayOpen = TimeField("Friday Open", validators=[DataRequired()])
+    FridayOpen = TimeField("Friday Close", validators=[DataRequired()])
+    SaturdayOpen = TimeField("Saturday Open", validators=[DataRequired()])
+    SaturdayOpen = TimeField("Saturday Close", validators=[DataRequired()])
+    Submit = SubmitField("Submit")
