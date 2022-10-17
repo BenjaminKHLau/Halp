92ee2177c5e3eeaa936e1b83dd15dbbdd717120a
Benjamin Lau <benjaminkhlau@gmail.com>
2022-10-16 22:28:12 -0500

    started business form, reducers, thunks, etc

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,7 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
+import BusinessFormComponent from "./components/businesses/businessform.js"
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -43,6 +44,9 @@ function App() {
         <ProtectedRoute path='/' exact={true} >
           <h1>My Home Page</h1>
         </ProtectedRoute>
+        <Route path="/businesses/new" exact={true}>
+          <BusinessFormComponent />
+          </Route>
       </Switch>
     </BrowserRouter>
   );
