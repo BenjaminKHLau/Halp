cc4e25593450e0e7d6c34909b244b8d7d500c6b5
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-19 18:35:12 -0500

    styled some buttons

--- react-app/src/components/businesses/businessCard.js
+++ react-app/src/components/businesses/businessCard.js
@@ -1,27 +1,20 @@
 import { Link } from "react-router-dom";
-import './businessCard.css'
+import "./businessCard.css";
 
-
-function BusinessCardComponent({ business }){
-    return (
-        <div className="Business-card">
-            <Link to={`/businesses/${business.id}`}>
-
-            <div className="Business-name">
-
-                {/* <div className="business-image-container"> */}
-                    <img src={business.business_image_url} className="business-image"/>
-                    {/* </div> */}
-                <div className="biz-name">{business.name}</div>
-                <div className="biz-address">{business.address}</div>
-                <div className="biz-city">{business.city}</div>
-                <div className="biz-state">{business.state}</div>
-
-
-            </div>
-            </Link>
+function BusinessCardComponent({ business }) {
+  return (
+      <div className="Business-card">
+        <div className="Business-name">
+          {/* <div className="business-image-container"> */}
+          <img src={business.business_image_url} className="business-image" />
+          {/* </div> */}
+          <div className="biz-name">{business.name}</div>
+          <div className="biz-address">{business.address}</div>
+          <div className="biz-city">{business.city}</div>
+          <div className="biz-state">{business.state}</div>
         </div>
-    )
+      </div>
+  );
 }
 
-export default BusinessCardComponent
+export default BusinessCardComponent;
