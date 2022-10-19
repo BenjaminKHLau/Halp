9d9037a9898c8952979457363d02870fef963e9c
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-19 10:31:12 -0500

    cateogors table and seeds implemented

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.business_routes import business_blueprint
 from .api.review_routes import review_blueprint
+from .api.category_routes import category_blueprint
 
 from .seeds import seed_commands
 
@@ -35,6 +36,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(business_blueprint, url_prefix='/api/businesses')
 app.register_blueprint(review_blueprint, url_prefix="/api/reviews")
+app.register_blueprint(category_blueprint, url_prefix="/api/categories")
 db.init_app(app)
 Migrate(app, db)
 
--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .businesses import seed_businesses, undo_businesses
+from .categories import seed_categories, undo_categories
 # , seed_business_images, undo_business_images, seed_categories, undo_categories
 
 # Creates a seed group to hold our commands
@@ -13,6 +14,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_businesses()
+    seed_categories()
     # seed_business_images()
     # seed_categories()
     # Add other seed functions here
@@ -23,6 +25,7 @@ def seed():
 def undo():
     undo_users()
     undo_businesses()
+    undo_categories()
     # undo_business_images()
     # undo_categories()
     # Add other undo functions here
