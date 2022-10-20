c48e245441cb05ec7b48f44491b61f744f9b74e2
Kyle Kassen <kyle.kassen@gmail.com>
2022-10-20 12:27:17 -0500

    search functioning

--- react-app/src/components/BusinessSearch.js
+++ react-app/src/components/BusinessSearch.js
@@ -1,35 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, Redirect, NavLink } from "react-router-dom";
+import { Link, Redirect, NavLink, useLocation } from "react-router-dom";
+import BusinessCardComponent from "./businesses/businessCard";
 
 function BusinessSearchComponent() {
+    const [businesses, setBusinesses] = useState([])
+    const location = useLocation()
     let url = new URL(window.location.href)
     const searchParams = url.searchParams
-    console.log(url, searchParams)
+
     const category = searchParams.get('category')
     const searchInput = searchParams.get('input')
 
-    console.log('qqqqqqqqq', new URL(window.location.href))
-    console.log(category)
-    console.log(searchInput)
-
     const search = category ? category : searchInput;
-    console.log(search)
+
 
     let test = async () => {
         let response = await fetch(`/api/businesses/query?search=${search}`)
-        console.log('response is: ', response)
-        let result = await response.json()
-        console.log('result is: ', result)
-        return result
+        setBusinesses(await response.json())
     }
 
-    console.log('test is', test())
+    useEffect(() => {
+        test()
+    }, [location])
+
+    console.log('businesses is:', businesses)
 
 
     return (
         <>
-            <h1>Test</h1>
+            {businesses.length ? <h1>Search Results For: {search}</h1> : <h1>No Results</h1>}
+            {console.log("------------------------")}
+            {businesses.map((business) => {
+                return <div className="businesses">
+                    <Link to={`/businesses/${business.id}`}>
+                    <BusinessCardComponent business={business} />
+                    {console.log('busineasfdasdfass is :', business)}
+                    </Link>
+                </div>
+            })}
         </>
     )
 }
