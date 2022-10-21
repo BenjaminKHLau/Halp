1b43f6bff5b81e7594ae5c4e985c1f268bff5895
Randy <randychang97@gmail.com>
2022-10-21 13:10:24 -0700

    review

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -91,6 +91,70 @@ def seed_reviews():
         "userId": 5,
         "imageUrl": "https://i0.wp.com/www.yogabasics.com/yogabasics2017/wp-content/uploads/2014/12/gentle-yoga-class.jpeg?fit=1080%2C720&amp;ssl=1"
     },
+    {
+        "stars": 5,
+        "review": "Good chicken, never experienced it before.",
+        "businessId": 5,
+        "userId": 1,
+        "imageUrl": "https://easychickenrecipes.com/wp-content/uploads/2019/06/featured-fried-chicken-sandwich-recipe-reshoot.jpg"
+    },
+    {
+        "stars": 2,
+        "review": "Chicken is boring meat",
+        "businessId": 5,
+        "userId": 3,
+        "imageUrl": "https://www.indianhealthyrecipes.com/wp-content/uploads/2016/07/chicken-sandwich-with-mayo-500x500.jpg"
+    },
+    {
+        "stars": 4,
+        "review": "Would love to come again and again!!",
+        "businessId": 5,
+        "userId": 2,
+        "imageUrl": "https://charlotte.axios.com/wp-content/uploads/2021/01/FullSizeRender-2-scaled.jpeg"
+    },
+    {
+        "stars": 1,
+        "review": "Very overcooked, not very welcoming either",
+        "businessId": 6,
+        "userId": 4,
+        "imageUrl": "https://www.visitpasadena.com/imager/s3_us-west-1_amazonaws_com/pasadena-2020/images/Food-Drink/dirt-dog4_c3cc3d3e99a0496ea9909ca43528d03d.jpg"
+    },
+    {
+        "stars": 2,
+        "review": "Could be better, the wait was so long",
+        "businessId": 6,
+        "userId": 3,
+        "imageUrl": "https://media.timeout.com/images/103397051/image.jpg"
+    },
+    {
+        "stars": 5,
+        "review": "This place is just more delicious each bite",
+        "businessId": 6,
+        "userId": 2,
+        "imageUrl": "https://static.wixstatic.com/media/1dd8f8_8789f5e1b6b94ae4b089519473452122~mv2.jpg/v1/fill/w_640,h_512,fp_0.50_0.50,q_80,usm_0.66_1.00_0.01,enc_auto/1dd8f8_8789f5e1b6b94ae4b089519473452122~mv2.jpg"
+    },
+    {
+        "stars": 1,
+        "review": "Why are the employees so obnoxious here??",
+        "businessId": 7,
+        "userId": 3,
+        "imageUrl": "https://image.cnbcfm.com/api/v1/image/106076143-1565697934801compaq-laptop-used-by-jennifer-aniston-rachel-green-and-bloomingdales-big-brown-bags-in-friends-season-3-episode-12-1.jpg?v=1565711838"
+    },
+    {
+        "stars": 3,
+        "review": "Found what I needed, but nothing special really.",
+        "businessId": 7,
+        "userId": 2,
+        "imageUrl": "https://www.fashionbeans.com/wp-content/uploads/2018/03/belts-18-top-luca.jpg"
+    },
+    {
+        "stars": 5,
+        "review": "This store is about to get me into caring about clothing!",
+        "businessId": 7,
+        "userId": 7,
+        "imageUrl": "https://www.northern-scot.co.uk/_media/img/750x0/Z84PPV5HYSPG0USCLX3W.jpg"
+    },
+
     ]
     for review in reviewsArray:
         newReview = Review(
